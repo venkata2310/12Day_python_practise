7b97422abc2c38268e6460714690ca42a2defcbe
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-22 21:15:03 +0530

    Add files via upload

---
+++ python_practise.py
@@ -0,0 +1,19 @@
+# z= 7+10j
+# print(type(z)) # complex
+
+# q={}
+# print(type(q)) # dict
+
+# a={1,2}
+# print(type(a)) #set
+
+# a= {1:'hello', 2:'hi', 3:'python'}
+# print(type(a))
+# print(tuple(a))
+# print(a[1])
+# print(a[2])
+# print(a[3])
+
+# z= (b'pythonworld')
+# y=bytearray(z)
+# print(type(y))
